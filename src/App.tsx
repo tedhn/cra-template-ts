740ea54500f50c97076b7878205a1a3824be911a
ted <heinhtetnaing186@gmail.com>
2020-09-25 18:47:46 +0630

    react-router-testing2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import i18n from "./locales/i18n";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+  Switch,
+} from "react-router-dom";
 
 const App: React.FC = () => {
   const { t } = useTranslation();
@@ -9,12 +18,42 @@ const App: React.FC = () => {
     i18n.changeLanguage(lng);
   };
 
+  const A = () => {
+    let match = useRouteMatch();
+    console.log(match.url);
+
+    return (
+      <div>
+        Compoenent A
+        <Router>
+          <Switch>
+            <Route path={`${match.url}/manage/`} exact component={D} />
+            <Route path={`${match.url}/:token`} exact component={C} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  };
+
+  const B = () => <div>Compoenent B</div>;
+  const C = () => {
+    interface Param {
+      token: string;
+    }
+
+    let param = useParams<Param>();
+    console.log(param.token);
+
+    return <div>Compoenent C</div>;
+  };
+  const D = () => <div>Compoenent D</div>;
+
   return (
-    <>
-      <div>{t("Welcome to React")}</div>
-      <button onClick={() => changeLanguage("de")}>de</button>
-      <button onClick={() => changeLanguage("en")}>en</button>
-    </>
+    <Router>
+      <Route path="/" exact component={A} />
+      <Route path="/website" component={A} />
+      <Route path="/dashboard" component={B} />
+    </Router>
   );
 };
 
